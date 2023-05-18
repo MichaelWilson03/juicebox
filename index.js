a8bf992adf1640f3419baf9c3458addb0ac04f8d
MichaelWilson03 <michaelbwilson2012@gmail.com>
2023-05-17 21:49:25 -0500

    idk1

---
+++ index.js
@@ -0,0 +1,8 @@
+const PORT = 5432;
+const express = require("express");
+const server = express();
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
