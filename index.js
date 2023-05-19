4f3a3a36cd431f635dd9a9a2284585d64361e713
MichaelWilson03 <michaelbwilson2012@gmail.com>
2023-05-18 23:05:02 -0500

    part 1

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
-const PORT = 5432;
+const PORT = 3000;
 const express = require("express");
 const server = express();
 const apiRouter = require("./api");
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+server.use(express.json());
 console.log(process.env.JWT_SECRET);
 
 server.use("/api", apiRouter);
+
+const { client } = require("./db");
+client.connect();
+
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
 });
