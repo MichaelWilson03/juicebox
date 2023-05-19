375b3106d66b7ec533417144ef14c1e07215be19
MichaelWilson03 <michaelbwilson2012@gmail.com>
2023-05-18 22:10:14 -0500

    idk

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const PORT = 5432;
 const express = require("express");
 const server = express();
 const apiRouter = require("./api");
+console.log(process.env.JWT_SECRET);
+
 server.use("/api", apiRouter);
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
